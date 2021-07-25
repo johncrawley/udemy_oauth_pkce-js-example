2e4be3c815338c2267277af06e561fb088d8b08e
johncrawley <johncrawley1@gmail.com>
2021-07-25 23:02:19 +0100

    Initial commit.

---
+++ src/main/java/com/jcrawley/udemyoauthpkce/PkceJsExampleApplication.java
@@ -0,0 +1,13 @@
+package com.jcrawley.udemyoauthpkce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkceJsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkceJsExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jcrawley/udemyoauthpkce/PkceJsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcrawley.udemyoauthpkce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkceJsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
